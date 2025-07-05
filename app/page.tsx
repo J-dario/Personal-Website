5d5a31cba80ce0d889577c33e8d7d58ae26dbb7d
Javier Dario <dariojavier485@gmail.com>
2025-07-05 04:33:45 -0400

    added sections and formatting

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,11 @@ export default function Home() {
     lofi: useRef<HTMLAudioElement>(null),
   };
   const [muted, setMuted] = useState(true);
+const section2Ref = useRef<HTMLDivElement>(null);
+
+const scrollToSection2 = () => {
+  section2Ref.current?.scrollIntoView({ behavior: 'smooth' });
+};
 
   const toggleMute = () => {
     setMuted((prev) => {
@@ -42,80 +47,66 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
 
-        <audio ref={audioRefs.waves} autoPlay preload="auto" loop muted>
-          <source src="/waves.mp3" type="audio/mpeg" />
-        </audio>
+        {/* Audio and UI */}
+        <audio ref={audioRefs.waves} autoPlay preload="auto" loop muted><source src="/waves.mp3" type="audio/mpeg" /></audio>
+        <audio ref={audioRefs.lofi} autoPlay preload="auto" loop muted><source src="/lofi.mp3" type="audio/mpeg" /></audio>
+        <button onClick={toggleMute} className={styles.muteButton} aria-label="Toggle Mute"><Image src={muted ? "/Volume X.svg" : "/Volume Icon.svg"} alt={muted ? "Muted" : "Unmuted"} width={64} height={64}/></button>
+        <button onClick={scrollToSection2} className={styles.downButton} aria-label="To Next Page"><Image src={"/Next.svg"} alt={"Next Page"} width={64} height={64}/></button>
 
-        <audio ref={audioRefs.lofi} autoPlay preload="auto" loop muted>
-          <source src="/lofi.mp3" type="audio/mpeg" />
-        </audio>
-
-        <button onClick={toggleMute} className={styles.muteButton} aria-label="Toggle Mute">
-          <Image src={muted ? "/Volume X.svg" : "/Volume Icon.svg"} alt={muted ? "Muted" : "Unmuted"} width={64} height={64}/>
-        </button>
-
-        <button onClick={toggleMute} className={styles.downButton} aria-label="To Next Page">
-          <Image src={"/Next.svg"} alt={"Next Page"} width={64} height={64}/>
-        </button>
-
-        <div className={styles.backgroundClouds}/>
-
-        <div className={styles.moonContainer}>
-          <div className={styles.moonRing2}>
-            <Image src="/Spokes.svg" alt="Spokes" width={800} height={800}/>
+        {/* Landing Page */}
+        <section className={styles.section}>
+          <div className={styles.content}>
+            <div className={styles.backgroundClouds}/>
+            <div className={styles.moonContainer}>
+              <div className={styles.moonRing2}><Image src="/Spokes.svg" alt="Spokes" width={800} height={800}/></div>
+              <div className={styles.moon}><Image src="/Moon Center.svg" alt="Moon" width={600} height={600}/></div>
+              <div className={styles.moonRing1}><Image src="/Moon Ring 1.svg" alt="MoonPhases" width={750} height={750}/></div>
+              <div className={styles.moonRing2}><Image src="/Moon Ring 2.svg" alt="MoonPhases" width={900} height={900}/></div>
+            </div>
+            <div className={styles.frontClouds}/>
+            <div className={styles.textContainer}>
+              <h1 className={styles.titleText}> Hi, I'm Javier </h1>
+              <h4 className={styles.subtitleText}> student, artist, developer </h4>
+            </div>
+            <div className={styles.waterContainer}>
+              <div>
+                <Wave fill='url(#waveOutline)' paused={false} options={{height: 95, amplitude: 15, speed: 0.15, points: 3}}>
+                  <defs>
+                    <linearGradient id="waveOutline">
+                      <stop offset="0%"  stopColor="#484A77" />
+                      <stop offset="25%" stopColor="#4D9BE6" />
+                      <stop offset="50%" stopColor="#8FD3FF" />
+                      <stop offset="75%" stopColor="#4D9BE6" />
+                      <stop offset="100%"  stopColor="#484A77" />
+                    </linearGradient>
+                  </defs>
+                </Wave>
+              </div>
+              <div className={styles.waterWave}>
+                <Wave fill='url(#waveFill)' paused={false} options={{height: 100, amplitude: 15, speed: 0.15, points: 3}}>
+                  <defs>
+                    <linearGradient id="waveFill">
+                      <stop offset="10%"  stopColor="#4D9BE6" />
+                      <stop offset="20%" stopColor="#A884F3" />
+                      <stop offset="40%"  stopColor="#C7DCD0" />
+                      <stop offset="45%" stopColor="#DEDEDE" />
+                      <stop offset="55%"  stopColor="#DEDEDE" />
+                      <stop offset="60%" stopColor="#C7DCD0" />
+                      <stop offset="80%"  stopColor="#A884F3" />
+                      <stop offset="90%" stopColor="#4D9BE6" />
+                    </linearGradient>
+                  </defs>
+                </Wave>
+              </div>
+              <div className={styles.waterFill}></div>
+            </div>
           </div>
-          <div className={styles.moon}>
-            <Image src="/Moon Center.svg" alt="Moon" width={600} height={600}/>
-          </div>
-          <div className={styles.moonRing1}>
-            <Image src="/Moon Ring 1.svg" alt="MoonPhases" width={750} height={750}/>
-          </div>
-          <div className={styles.moonRing2}>
-            <Image src="/Moon Ring 2.svg" alt="MoonPhases" width={900} height={900}/>
-          </div>
-        </div>
-
-        <div className={styles.frontClouds}/>
-
-        <div className={styles.textContainer}>
-          <h1 className={styles.titleText}> Hi, I'm Javier </h1>
-          <h4 className={styles.subtitleText}> student, artist, developer </h4>
-        </div>
-
-        <div className={styles.waterContainer}>
-
-          <div>
-            <Wave fill='url(#waveOutline)' paused={false} options={{height: 95, amplitude: 15, speed: 0.15, points: 3}}>
-              <defs>
-                <linearGradient id="waveOutline">
-                  <stop offset="0%"  stopColor="#484A77" />
-                  <stop offset="25%" stopColor="#4D9BE6" />
-                  <stop offset="50%" stopColor="#8FD3FF" />
-                  <stop offset="75%" stopColor="#4D9BE6" />
-                  <stop offset="100%"  stopColor="#484A77" />
-                </linearGradient>
-              </defs>
-            </Wave>
-          </div>
-
-          <div className={styles.waterWave}>
-            <Wave fill='url(#waveFill)' paused={false} options={{height: 100, amplitude: 15, speed: 0.15, points: 3}}>
-              <defs>
-                <linearGradient id="waveFill">
-                  <stop offset="10%"  stopColor="#4D9BE6" />
-                  <stop offset="20%" stopColor="#A884F3" />
-                  <stop offset="40%"  stopColor="#C7DCD0" />
-                  <stop offset="45%" stopColor="#DEDEDE" />
-                  <stop offset="55%"  stopColor="#DEDEDE" />
-                  <stop offset="60%" stopColor="#C7DCD0" />
-                  <stop offset="80%"  stopColor="#A884F3" />
-                  <stop offset="90%" stopColor="#4D9BE6" />
-                </linearGradient>
-              </defs>
-            </Wave>
-          </div>
-          <div className={styles.waterFill}></div>
-        </div>
+        </section>
+        
+        {/* Construction Page */}
+        <section className={styles.section} ref={section2Ref}>Section 2
+          
+        </section>
       </main>
     </div>
   );
