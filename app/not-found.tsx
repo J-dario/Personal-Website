39132f663a8d11a3e6824c52d7ece8bdc616d890
Javier Dario <dariojavier485@gmail.com>
2025-07-07 03:42:23 -0400

    updated 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,21 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import Link from 'next/link';
+
 export default function NotFound() {
     return (
-        <div>
-            <h2>404 Page Not Found</h2>
-            <p>Take me back to safety</p>
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <div className={styles.four04}>
+                    <div className={styles.moonContainer}>
+                        <div><Image src="/Moon Center.svg" alt="Moon" width={400} height={400}/></div>
+                    </div>
+                    <h2 className={styles.four}>4&nbsp;&nbsp;&nbsp;&nbsp;4</h2>
+                </div>
+                <Link href="/">
+                  <p className={styles.four04TxT}>Take me back</p>
+                </Link>
+            </main>
         </div>
     );
 }
